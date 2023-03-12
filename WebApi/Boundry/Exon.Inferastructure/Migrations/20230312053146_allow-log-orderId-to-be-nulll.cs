00adff220d3a89d3564b54d0eeea24a0efe641e8
Mahyar Golbaz sourkhani <mahyar.golbaz@gmail.com>
2023-03-12 09:06:13 +0330

    add log order id nullable

---
+++ WebApi/Boundry/Exon.Inferastructure/Migrations/20230312053146_allow-log-orderId-to-be-nulll.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Exon.Inferastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class allowlogorderIdtobenulll : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "orderId",
+                table: "Logs",
+                newName: "OrderId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OrderId",
+                table: "Logs",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "OrderId",
+                table: "Logs",
+                newName: "orderId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "orderId",
+                table: "Logs",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
